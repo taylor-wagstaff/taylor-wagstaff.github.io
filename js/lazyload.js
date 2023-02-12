319f61657c7dd75747f7ed49c13b4482c6626d79
Taylor Wagstaff <taylorwagstaff@gmail.com>
2023-02-13 11:32:28 +1300

    test2

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,8 +1,8 @@
-import { URL } from './url'
-
-var imageUrlCollection = URL
-console.log('url', URL)
-var parentElement = document.getElementById('test')
+var imageUrlCollection = new Array(
+  'https://d2w9rnfcy7mm78.cloudfront.net/20135989/original_4f8b921a89b85bfd4f38f0a19263a408.png?1675178083?bc=0',
+  'https://d2w9rnfcy7mm78.cloudfront.net/9711151/original_5a60a451b02be6c3ba34f907c71620f6.png?1606768190?bc=0'
+) //image URL collection
+var parentElement = document.getElementById('comicPages')
 for (var item = 0; item < imageUrlCollection.length; item++) {
   var image = document.createElement('img')
   image.id = item
