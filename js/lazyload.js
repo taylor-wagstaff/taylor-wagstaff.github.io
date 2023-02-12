c00eb0d48b6ca43b54f035273d9257e9c5bfe97e
Taylor Wagstaff <taylorwagstaff@gmail.com>
2023-02-13 11:57:40 +1300

    new

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,17 +1,4 @@
-var imageUrlCollection = new Array(
-  'https://d2w9rnfcy7mm78.cloudfront.net/20135989/original_4f8b921a89b85bfd4f38f0a19263a408.png?1675178083?bc=0',
-  'https://d2w9rnfcy7mm78.cloudfront.net/9711151/original_5a60a451b02be6c3ba34f907c71620f6.png?1606768190?bc=0'
-) //image URL collection
-var parentElement = document.getElementById('comicPages')
-for (var item = 0; item < imageUrlCollection.length; item++) {
-  var image = document.createElement('img')
-  image.id = item
-  image.className = 'img'
-  image.src = imageUrlCollection[item]
-  parentElement.appendChild(image)
-}
 
-//////
 document.addEventListener('DOMContentLoaded', function () {
   var lazyloadImages = document.querySelectorAll('img.lazy')
   var lazyloadThrottleTimeout
--- js/url.js
+++
@@ -1,6 +0,0 @@
-const URL = [
-  'https://d2w9rnfcy7mm78.cloudfront.net/20135989/original_4f8b921a89b85bfd4f38f0a19263a408.png?1675178083?bc=0',
-  'https://d2w9rnfcy7mm78.cloudfront.net/9711151/original_5a60a451b02be6c3ba34f907c71620f6.png?1606768190?bc=0',
-]
-
-export { URL }
